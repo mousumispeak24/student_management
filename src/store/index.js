71ba1bc7f8bee4c08f74f643fa57bfbe2f174474
mousumi <mousumi@emproto.com>
2021-04-18 14:23:47 +0530

    __REDUX_DEVTOOLS_EXTENSION__ removed

--- src/store/index.js
+++ src/store/index.js
@@ -8,8 +8,8 @@ const configureStore = () => {
     rootReducer,
     compose(
       applyMiddleware(sagaMiddleware),
-      window.__REDUX_DEVTOOLS_EXTENSION__ &&
-        window.__REDUX_DEVTOOLS_EXTENSION__()
+      // window.__REDUX_DEVTOOLS_EXTENSION__ &&
+      //   window.__REDUX_DEVTOOLS_EXTENSION__()
     )
   );
 
